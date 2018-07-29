783b2382ad083acbcad86008c1c6d5ce87ca6bb8
Newbie251998 <banaresjoshua25@gmail.com>
2018-07-29 09:47:03 +0800

    Todo tutorial

---
+++ resources/views/todos.blade.php
@@ -0,0 +1,23 @@
+@extends('layout')
+
+@section('content')
+    <div class="row">
+        <div class="col-lg-6 col-lg-offset-3">
+            <form action="/create/todo" method="post">
+                {{ csrf_field() }}
+                <input type="text" class="form-control input-lg" name="todo" placeholder="Create a new todo">
+            </form>
+        </div>
+    </div>
+    <hr>
+    @foreach($todos as $todo)
+        {{ $todo->todo }} <a href="{{ route('todo.delete', ['id' => $todo->id]) }}" class="btn btn-danger">Delete todo</a>
+        <a href="{{ route('todo.update', ['id' => $todo->id]) }}" class="btn btn-info btn-xs">Update</a>
+        @if(!$todo->completed)
+        <a href="{{ route('todos.completed', ['id' => $todo->id]) }}" class="btn btn-success">Complete</a>
+        @else
+        <span class="text-success">Completed</span>
+        @endif
+        <hr>
+    @endforeach
+@stop
